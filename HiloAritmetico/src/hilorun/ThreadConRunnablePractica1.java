6523d4bf13c69bafb6749e0905e328eaa0daea4a
MrSchopenhauer <jose.uniriva15@gmail.com>
2023-08-16 14:05:06 -0600

    Se finalizo el programa
    El programa ya funciona a traves de un cambio de perspectiva en la manera en la que se
    construye el metodo de los hilos. EL problema fue el entendimiento de la manera de
    activar mismo metodo para ambos. Es un mismo proceso, dos hilos. Un metodo por hilo podria
    interpretarse como un proceso por hilo?

--- HiloAritmetico/src/hilorun/ThreadConRunnablePractica1.java
+++ HiloAritmetico/src/hilorun/ThreadConRunnablePractica1.java
@@ -1,79 +1,65 @@
-/*
+/*]
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package hilorun;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  *
- * @author leledezma    FORK ---> JOIN  MAIN CREA A T1 y A T1 ----> join
+ * @author Antonio Rivera con template de Ledezma    FORK ---> JOIN  MAIN CREA A T1 y A T1 ----> join
  */
+
 class ThreadConRunnablePractica1 implements Runnable{
     int numero;
-    int resultado;
+    boolean whoIsWho;
     
-    public ThreadConRunnablePractica1 (int _numero){
+    public ThreadConRunnablePractica1 (int _numero, boolean _whoIsWho){
         numero = _numero;
+        whoIsWho = _whoIsWho;
     }
+    
     @Override
     public void run(){
-        for(int i=0;i<10;i++)              
-            System.out.print(palabra);
-    }
-    public void run(String a) {
+    	int resultadoSuma=numero;
+    	int resultadoFactorial=numero;
     	
+    	// Se designa la accion que hara cada hilo a traves de operador booleano
+    	if (whoIsWho) {
+    		
+    		//se define la operacion de suma si el hilo es verdadero
+    		for(int i= numero; i > 0;i = i-1) {
+    			resultadoSuma = resultadoSuma + i-1;
+    		}
+    		
+    	System.out.println("El resultado de la suma es: "+ resultadoSuma);
+    		
+    	}else{
+    		
+    		//se define la operacion de factorial si el hilo es falso
+    		for(int i= numero; i > 1 ;i--) {
+    			resultadoFactorial = resultadoFactorial*(i-1);
+    		}
+    		
+    		System.out.println("El resultado del factorial es: "+ resultadoFactorial);
+    	}
+    
     }
     
     public static void main(String args[]){
-    	String sonIguales;
+    	System.out.println("Cual numero quieres procesar? >>");
+    	Scanner enteroScanner = new Scanner(System.in);
+    	int respuesta = enteroScanner.nextInt();
     	
-    	try {
-    	System.out.println("Bienvenido."+"/n"+"Deseas procesar ambas operaciones con un mismo numero? Contesta con y o n");
-    	Scanner inputScanner = new Scanner(System.in);
-    	sonIguales = inputScanner.next();
-    	while(sonIguales != "y" || sonIguales != "n") {
-    		System.out.println("Debes contestar con y o n, en minuscula");
-    	}
-    	}catch(InputMismatchException a) {
-    		System.out.println("Debes contestar con y o n, en minuscula");
-    	}
-
-    	
-    	switch(sonIguales){
-    	int valorA, valorB;
-    	
-    	case "y":
-        	try {
-            	System.out.println("Que numero deseas utilizar?");
-            	Scanner inputScanner = new Scanner(System.in);
-            	valorA = inputScanner.hasNextInt();
-            	Thread t1 = new Thread(a);
-                Thread t2 = new Thread(b);
-                t1.start();
-                t2.start();
-                System.out.print("Fin del programa principal\n");
-
-            	}
-            	catch(InputMismatchException a) {
-            		System.out.println("Solo numeros enteros");
-            	}
-    	ThreadConRunnablePractica1 a = new ThreadConRunnablePractica1(valorA);
-        ThreadConRunnablePractica1 b = new ThreadConRunnablePractica1(valorA);
-    		
-    		
-    	case "n":
-    	
-    	
-    	}
-    
-    	
+        ThreadConRunnablePractica1 a = new ThreadConRunnablePractica1(respuesta,false);
+        ThreadConRunnablePractica1 b = new ThreadConRunnablePractica1(respuesta,true);
+        
         Thread t1 = new Thread(a);
         Thread t2 = new Thread(b);
         t1.start();
         t2.start();
         System.out.print("Fin del programa principal\n");
-    	
+        enteroScanner.close();
     }
 }
+
