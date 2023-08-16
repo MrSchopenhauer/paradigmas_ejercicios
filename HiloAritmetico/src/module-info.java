1d6791c7008eba836d02d59149ea9b5e89f16e4d
MrSchopenhauer <jose.uniriva15@gmail.com>
2023-08-15 21:39:03 -0600

    Adding files for the new class project
    Project about threads and its use in arithmetic. ALso, a starting NewtonRaphson algorithm

---
+++ HiloAritmetico/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author mrschopen
+ *
+ */
+module HiloAritmetico {
+}
---
+++ NewtonRaphson/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+
+    public static void main(String args[]){
+        double valorInicial = 0.01;
+        double resultado = 0;
+        double diferenciaProporcional=1; 
+
+        while(diferenciaProporcional != 0){
+            double funcionNatural = 16*
+
+        }
+
+    }
+}
